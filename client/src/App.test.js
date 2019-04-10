6383bc37c464ac4ea19b795421e09add2fcdbc21
jordvaug <jordanvaughn1@gmail.com>
2019-04-10 14:34:19 -0600

    added first test assertions to react client

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import React from "react";
+import ReactDOM from "react-dom";
+import { configure, shallow } from "enzyme";
+import { expect } from "chai";
+import App from "./App";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+describe("App component testing", function() {
+  it("renders welcome message", function() {
+    const wrapper = shallow(<App />);
+    const welcome = (
+      <h1 className="text-white text-uppercase text-center my-4">
+        Trym Todo app
+      </h1>
+    );
+    expect(wrapper.contains(welcome)).to.equal(true);
+  });
 });
